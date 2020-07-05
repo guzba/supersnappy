ed4c9d59d18ecc25b638f41795c01f8c6e144690
Ryan Oldenburg <guzba8@gmail.com>
2020-07-05 00:33:24 -0500

    add benchmarks + chunks timing

---
+++ tests/benchmark.nim
@@ -0,0 +1,89 @@
+## nim c --gc:arc -d:release -r .\tests\benchmark.nim
+
+import ../supersnappy, nimsnappyc, snappy, std/monotimes, strformat
+
+const
+  files = [
+    "alice29.txt", "asyoulik.txt", "fireworks.jpg", "geo.protodata",
+    "html", "html_x_4", "kppkn.gtb", "lcet10.txt", "paper-100k.pdf",
+    "plrabn12.txt", "urls.10K"
+  ]
+  iterations = 1000
+
+block jangko_snappy:
+  echo "https://github.com/jangko/snappy"
+  for file in files:
+    let
+      original = cast[seq[uint8]](readFile(&"tests/data/{file}"))
+      start = getMonoTime().ticks
+    var
+      compressedLen = encode(original).len
+      c: int
+    for i in 0 ..< iterations:
+      let
+        compressed = encode(original)
+        uncompressed = decode(compressed)
+      inc(c, uncompressed.len)
+    let
+      delta = float64(getMonoTime().ticks - start) / 1000000000.0
+      percent = 100 - ((compressedLen / original.len) * 100)
+    echo &"  {file}: {delta:.4f}s (compressed to {compressedLen} bytes, {percent:.2f}% reduction) [{c}]"
+
+block nimcompression_nimsnappyc:
+  echo "https://github.com/NimCompression/nimsnappyc"
+  for file in files:
+    let
+      original = cast[seq[uint8]](readFile(&"tests/data/{file}"))
+      start = getMonoTime().ticks
+    var
+      compressedLen = snappyCompress(original).len
+      c: int
+    for i in 0 ..< iterations:
+      let
+        compressed = snappyCompress(original)
+        uncompressed = snappyUncompress(compressed)
+      inc(c, uncompressed.len)
+    let
+      delta = float64(getMonoTime().ticks - start) / 1000000000.0
+      percent = 100 - ((compressedLen / original.len) * 100)
+    echo &"  {file}: {delta:.4f}s (compressed to {compressedLen} bytes, {percent:.2f}% reduction) [{c}]"
+
+block guzba_supersnappy:
+  echo "https://github.com/guzba/supersnappy"
+  for file in files:
+    let
+      original = cast[seq[uint8]](readFile(&"tests/data/{file}"))
+      start = getMonoTime().ticks
+    var
+      compressedLen = supersnappy.compress(original).len
+      c: int
+    for i in 0 ..< iterations:
+      let
+        compressed = supersnappy.compress(original)
+        uncompressed = supersnappy.uncompress(compressed)
+      inc(c, uncompressed.len)
+    let
+      delta = float64(getMonoTime().ticks - start) / 1000000000.0
+      percent = 100 - ((compressedLen / original.len) * 100)
+    echo &"  {file}: {delta:.4f}s (compressed to {compressedLen} bytes, {percent:.2f}% reduction) [{c}]"
+
+block guzba_supersnappy_reuse:
+  echo "https://github.com/guzba/supersnappy [reuse]"
+  var
+    reuseCompress = newSeq[uint8]()
+    reuseUncompress = newSeq[uint8]()
+  for file in files:
+    let
+      original = cast[seq[uint8]](readFile(&"tests/data/{file}"))
+      start = getMonoTime().ticks
+    var
+      compressedLen = supersnappy.compress(original).len
+      c: int
+    for i in 0 ..< iterations:
+      compress(original, reuseCompress)
+      uncompress(reuseCompress, reuseUncompress)
+      inc(c, reuseUncompress.len)
+    let
+      delta = float64(getMonoTime().ticks - start) / 1000000000.0
+      percent = 100 - ((compressedLen / original.len) * 100)
+    echo &"  {file}: {delta:.4f}s (compressed to {compressedLen} bytes, {percent:.2f}% reduction) [{c}]"
---
+++ tests/chunks.nim
@@ -0,0 +1,82 @@
+## nim c --gc:arc -d:release -r .\tests\chunks.nim
+
+import ../supersnappy, nimsnappyc, os, snappy, std/monotimes, strformat
+
+const
+  iterations = 100
+
+var files: seq[string]
+for file in walkFiles("tests/data/chunks/*"):
+  files.add(file)
+
+block jangko_snappy:
+  echo "https://github.com/jangko/snappy"
+  var
+    totalUncompressed, totalCompressed: int
+    totalDelta: float64
+  for file in files:
+    let
+      original = cast[seq[uint8]](readFile(file))
+      start = getMonoTime().ticks
+    var
+      compressedLen = encode(original).len
+      c: int
+    inc(totalUncompressed, original.len)
+    inc(totalCompressed, compressedLen)
+    for i in 0 ..< iterations:
+      let
+        compressed = encode(original)
+        uncompressed = decode(compressed)
+      inc(c, uncompressed.len)
+    let delta = float64(getMonoTime().ticks - start) / 1000000000.0
+    totalDelta = totalDelta + delta
+  let percent = 100 - ((totalCompressed / totalUncompressed) * 100)
+  echo &"  {totalDelta:.4f}s total, {percent:.2f}% compression"
+
+block nimcompression_nimsnappyc:
+  echo "https://github.com/NimCompression/nimsnappyc"
+  var
+    totalUncompressed, totalCompressed: int
+    totalDelta: float64
+  for file in files:
+    let
+      original = cast[seq[uint8]](readFile(file))
+      start = getMonoTime().ticks
+    var
+      compressedLen = snappyCompress(original).len
+      c: int
+    inc(totalUncompressed, original.len)
+    inc(totalCompressed, compressedLen)
+    for i in 0 ..< iterations:
+      let
+        compressed = snappyCompress(original)
+        uncompressed = snappyUncompress(compressed)
+      inc(c, uncompressed.len)
+    let delta = float64(getMonoTime().ticks - start) / 1000000000.0
+    totalDelta = totalDelta + delta
+  let percent = 100 - ((totalCompressed / totalUncompressed) * 100)
+  echo &"  {totalDelta:.4f}s total, {percent:.2f}% compression"
+
+block guzba_supersnappy:
+  echo "https://github.com/guzba/supersnappy"
+  var
+    totalUncompressed, totalCompressed: int
+    totalDelta: float64
+  for file in files:
+    let
+      original = cast[seq[uint8]](readFile(file))
+      start = getMonoTime().ticks
+    var
+      compressedLen = supersnappy.compress(original).len
+      c: int
+    inc(totalUncompressed, original.len)
+    inc(totalCompressed, compressedLen)
+    for i in 0 ..< iterations:
+      let
+        compressed = supersnappy.compress(original)
+        uncompressed = supersnappy.uncompress(compressed)
+      inc(c, uncompressed.len)
+    let delta = float64(getMonoTime().ticks - start) / 1000000000.0
+    totalDelta = totalDelta + delta
+  let percent = 100 - ((totalCompressed / totalUncompressed) * 100)
+  echo &"  {totalDelta:.4f}s total, {percent:.2f}% compression"
