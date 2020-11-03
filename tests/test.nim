3f7ebb61eb97cca6622461482a641b32ee6a84e3
Ryan Oldenburg <guzba8@gmail.com>
2020-11-02 22:09:26 -0600

    doAssert

--- tests/test.nim
+++ tests/test.nim
@@ -3,21 +3,21 @@ import strformat
 include ../supersnappy
 
 block varint:
-  assert varint(0) == ([0.uint8, 0, 0, 0, 0], 1)
-  assert varint(64) == ([0x40.uint8, 0, 0, 0, 0], 1)
-  assert varint(2097150) == ([0xFE.uint8, 0xFF, 0x7F, 0, 0], 3)
-  assert varint(high(uint32)) == ([0xFF.uint8, 0xFF, 0xFF, 0xFF, 0x0F], 5)
-  assert varint(0xFF.uint8) == ([0xFF.uint8, 0x01, 0, 0, 0], 2)
+  doAssert varint(0) == ([0.uint8, 0, 0, 0, 0], 1)
+  doAssert varint(64) == ([0x40.uint8, 0, 0, 0, 0], 1)
+  doAssert varint(2097150) == ([0xFE.uint8, 0xFF, 0x7F, 0, 0], 3)
+  doAssert varint(high(uint32)) == ([0xFF.uint8, 0xFF, 0xFF, 0xFF, 0x0F], 5)
+  doAssert varint(0xFF.uint8) == ([0xFF.uint8, 0x01, 0, 0, 0], 2)
 
-  assert varint(varint(0)[0]) == (0.uint32, 1)
-  assert varint(varint(64)[0]) == (64.uint32, 1)
-  assert varint(varint(2097150)[0]) == (2097150.uint32, 3)
-  assert varint(varint(high(uint32))[0]) == (high(uint32), 5)
-  assert varint(varint(0xFF.uint8)[0]) == (255.uint32, 2)
+  doAssert varint(varint(0)[0]) == (0.uint32, 1)
+  doAssert varint(varint(64)[0]) == (64.uint32, 1)
+  doAssert varint(varint(2097150)[0]) == (2097150.uint32, 3)
+  doAssert varint(varint(high(uint32))[0]) == (high(uint32), 5)
+  doAssert varint(varint(0xFF.uint8)[0]) == (255.uint32, 2)
 
-  assert varint([0xFF.uint8, 0xFF, 0xFF, 0xFF, 0xFF]) == (0.uint32, 0) # Overflows
-  assert varint([0xFF.uint8]) == (0.uint32, 0) # Invalid encoding
-  assert varint([]) == (0.uint32, 0) # Invalid encoding
+  doAssert varint([0xFF.uint8, 0xFF, 0xFF, 0xFF, 0xFF]) == (0.uint32, 0) # Overflows
+  doAssert varint([0xFF.uint8]) == (0.uint32, 0) # Invalid encoding
+  doAssert varint([]) == (0.uint32, 0) # Invalid encoding
 
 block baddata:
   for i in 1 .. 3:
@@ -38,19 +38,19 @@ for file in files:
     original = readFile(&"tests/data/{file}")
     compressed = compress(original)
     uncompressed = uncompress(compressed)
-  assert uncompressed == original, &"Uncompressed != original for {file}"
+  doAssert uncompressed == original, &"Uncompressed != original for {file}"
 
 var reuseCompressed, reuseUncompressed: seq[uint8]
 for file in files:
   let original = cast[seq[uint8]](readFile(&"tests/data/{file}"))
   compress(original, reuseCompressed)
   uncompress(reuseCompressed, reuseUncompressed)
-  assert reuseUncompressed == original, &"Uncompressed != original for {file}"
+  doAssert reuseUncompressed == original, &"Uncompressed != original for {file}"
 
 for file in files:
   let
     original = readFile(&"tests/data/{file}")
     compressed = readFile(&"tests/data/{file}.snappy")
     uncompressed = uncompress(compressed)
-  assert uncompressed == original,
+  doAssert uncompressed == original,
     &"Uncompressed != original for {file} (snappy-c output)"
